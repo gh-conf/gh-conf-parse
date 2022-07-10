5898e38c3a6e68ac8b4db2f5eca8d0094e8eb20f
Arshad Kazmi <arshadkazmi42@gmail.com>
2022-07-10 21:22:47 +0530

    Add support for ssh origin

--- tests/test.js
+++ tests/test.js
@@ -4,15 +4,17 @@ const { ParseConfig, UserRepo } = require('../index');
 
 
 describe('validate parse config', () => {
-  it('should return correct config', () => {
-    const url = 'https://github.com/gh-conf/gh-conf-parse';
+  it('should return correct config with default', () => {
+    const http = 'https://github.com/gh-conf/gh-conf-parse';
+    const ssh = 'git@github.com:gh-conf/gh-conf-parse';
     const configUrl = ParseConfig()['remote "origin"'].url.replace('.git', '');
-    expect(configUrl).to.equal(url);
+    expect(configUrl).to.be.oneOf([http, ssh]);
   });
   it('should return correct config with input path', () => {
-    const url = 'https://github.com/gh-conf/gh-conf-parse';
+    const http = 'https://github.com/gh-conf/gh-conf-parse';
+    const ssh = 'git@github.com:gh-conf/gh-conf-parse';
     const configUrl = ParseConfig('.')['remote "origin"'].url.replace('.git', '');
-    expect(configUrl).to.equal(url);
+    expect(configUrl).to.be.oneOf([http, ssh]);
   });
 });
 describe('validates user repo', () => {
